b500977a3a1bd7be21009629108b8f9489d5556b
gyojincompany <luckyred@gmail.com>
2023-02-01 14:50:54 +0900

    글쓰기 view page 추가

---
+++ src/main/java/com/gyojincompany/board/controller/BoardController.java
@@ -0,0 +1,56 @@
+package com.gyojincompany.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardController
+ */
+@WebServlet("/*.do")
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();//컨택스트 패스만 분리하여 저장
+		String command = uri.substring(conPath.length());
+		
+		System.out.println("command : "+command);
+		
+		String viewPage = null;//view의 이름
+		
+		
+	}
+
+}
